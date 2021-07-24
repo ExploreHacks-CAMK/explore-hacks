82ff1d1c1841818b5abf4c8a86fe35b67a673356
Michael Chen <mkchen91021@gmail.com>
2021-07-24 16:48:01 -0700

    made canvas

--- game.js
+++ game.js
@@ -1,16 +1,10 @@
-const tabs = document.querySelectorAll('[data-tab-content]')
-var gameButton = document.getElementById("game-button");
-var backButton = document.getElementById("back-button")
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-gameButton.onclick = function() {
-    tabs.forEach(tab => {
-        tab.classList.remove('active')
-    });
-    document.querySelector('#game').classList.add('active');
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    
+    ctx.closePath();
 }
-backButton.onclick = function() {
-    tabs.forEach(tab => {
-        tab.classList.remove('active')
-    });
-    document.querySelector('#info').classList.add('active');
-}
+setInterval(draw, 50);
